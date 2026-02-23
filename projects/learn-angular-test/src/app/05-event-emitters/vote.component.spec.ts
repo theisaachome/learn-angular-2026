4b4b04070f1b5f46b272775c32772d8e2dd58a0d
theisaachome <65864160+theisaachome@users.noreply.github.com>
2026-02-23 23:42:32 +0800

    learn-angular-test 2-5 event emitter test.

--- projects/learn-angular-test/src/app/05-event-emitters/vote.component.spec.ts
+++ projects/learn-angular-test/src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,16 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when updated', () => {
+    let totalVotes = null;
+    component.voteChanged.subscribe(v => totalVotes = v);
+    component.upVote();
+    expect(totalVotes).toBe(1);
   });
 });
